18c4afa34f4284a7a7ea045fc600da0075b89be9
novakvova <novakvova@gmail.com>
2018-12-02 13:27:51 +0200

    Begin app Routes

---
+++ src/components/signup/SignupPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SignupPage extends React.Component {
+    render() {
+        return(
+            <h1>Sing up page</h1>
+        );
+    }
+}
+
+export default SignupPage;
